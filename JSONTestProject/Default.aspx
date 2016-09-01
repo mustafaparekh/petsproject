0aa4c77b354604cd451e72a1edd0c7521ff54e3a
mustafaparekh <mustafa_parekh@hotmail.com>
2016-09-01 23:42:48 +1000

    Add files via upload

---
+++ JSONTestProject/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h2>Male Gender</h2>
+    <asp:Literal ID="ltMale" runat="server"></asp:Literal>
+    </div>
+    
+    <br /><br /><br />
+    <div>
+        <h2>Female Gender</h2>
+    <asp:Literal ID="ltFemale" runat="server"></asp:Literal>
+    </div>
+    
+        <br /><br /><br />
+    <div>
+        <h2>Unknown Gender</h2>
+    <asp:Literal ID="ltUnknown" runat="server"></asp:Literal>
+    </div>
+    </form>
+</body>
+</html>
---
+++ JSONTestProject/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    string PET_TYPE_FILTER = "Cat";
+    
+    // Loads the JSON data and call main function to start processing
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string data = loadData(ConfigurationManager.AppSettings["jsonDataUrl"]);
+        startProcessing(data, PET_TYPE_FILTER);
+    }
+
+    // Loads the JSON data from remote URL 
+    private string loadData(string jsonUrl) 
+    {
+        WebClient webC = new WebClient();
+        return webC.DownloadString(jsonUrl);
+    }
+
+    // Take Json data and petTypeFilter as arguments, performs processing and prints the output
+    private void startProcessing(string data, string petTypeFilter) 
+    {
+        JArray jsonCollection = JArray.Parse(data);
+        Util u = new Util();
+        List<string> mGender = u.getDataForGenderGroup(jsonCollection, "Male", petTypeFilter);
+        List<string> fGender = u.getDataForGenderGroup(jsonCollection, "Female", petTypeFilter);
+        List<string> uGender = u.getDataForGenderGroup(jsonCollection, "Unknown", petTypeFilter);
+
+        ltMale.Text = String.Join("<br/>", mGender.ToArray());
+        ltFemale.Text = String.Join("<br/>", fGender.ToArray());
+        ltUnknown.Text = String.Join("<br/>", uGender.ToArray());
+    }
+}
